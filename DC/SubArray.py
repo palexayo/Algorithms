c8c36bb3df7a2db397aaabbd46103465b99ba087
palexayo <pablokesseli@hotmail.com>
2025-05-05 09:46:51 +0200

    Fixed bugs and optimized my algorithm which solves the maximum subarray problem

--- DC/SubArray.py
+++ DC/SubArray.py
@@ -1,86 +1,58 @@
 import random
 
+
 class MaxTeilSum:
     def findMaxSubArray(self, inputList):
-        if (len(inputList) == 1):
-            if (inputList[0] < 0):
-                return 0
-            else:
-                return inputList[0]
-        else:
-            dividedListTouple = self.divide(inputList)
-            maxLAndRTouple = self.conquer(dividedListTouple)
-            rightBorderMax = self.getListLeftRightBorderMax(inputList, len(dividedListTouple[0]))
-            leftBorderMax = self.getListRightLeftBorderMax(inputList, len(dividedListTouple[0]))
-            maxsum = self.merge(maxLAndRTouple[0], maxLAndRTouple[1], rightBorderMax, leftBorderMax)
-            return maxsum
+        # Base case: single element
+        if len(inputList) == 1:
+            return inputList[0]
 
-    def divide(self, inputList):
-        inputSize = len(inputList) // 2
-        listLower = inputList[:inputSize]
-        listUpper = inputList[inputSize:]
+        # Divide the list
+        mid = len(inputList) // 2
+        left_list = inputList[:mid]
+        right_list = inputList[mid:]
 
-        return listLower, listUpper
+        # Conquer step
+        max_left = self.findMaxSubArray(left_list)
+        max_right = self.findMaxSubArray(right_list)
 
-    def conquer(self, inputTouple):
-        listLeft = inputTouple[0]
-        maxL = self.findMaxSubArray(listLeft)
+        # Find max crossing sum
+        max_cross = self._maxCrossingSum(inputList, mid)
 
-        listRight = inputTouple[1]
-        maxR = self.findMaxSubArray(listRight)
+        # Return the maximum of the three
+        return max(max_left, max_right, max_cross)
 
-        return maxL, maxR
+    def _maxCrossingSum(self, inputList, mid):
+        # Left of mid
+        left_sum = float('-inf')
+        current_sum = 0
+        for i in range(mid - 1, -1, -1):
+            current_sum += inputList[i]
+            if current_sum > left_sum:
+                left_sum = current_sum
 
-    def getListLeftRightBorderMax(self, list, splitSize):
-        rightBorderMax = 0
-        sum = 0
+        # Right of mid
+        right_sum = float('-inf')
+        current_sum = 0
+        for i in range(mid, len(inputList)):
+            current_sum += inputList[i]
+            if current_sum > right_sum:
+                right_sum = current_sum
 
-        for i in range(splitSize - 1, -1, -1):
-            sum += list[i]
-            if rightBorderMax < sum:
-                rightBorderMax = sum
-        return rightBorderMax
-
-    def getListRightLeftBorderMax(self, list, splitSize):
-        leftBorderMax = 0
-        sum = 0
-
-        for i in range(splitSize, len(list)):
-            sum += list[i]
-            if leftBorderMax < sum:
-                leftBorderMax = sum
-        return leftBorderMax
-
-    def merge(self, maxL, maxR, rightBorderMax, leftBorderMax):
-        maxsum = 0
-        if maxL > maxR:
-            if maxL > rightBorderMax + leftBorderMax:
-                maxsum = maxL
-            else:
-                maxsum = rightBorderMax + leftBorderMax
-        else:
-            if maxR > rightBorderMax + leftBorderMax:
-                maxsum = max
-            else:
-                maxsum = rightBorderMax + leftBorderMax
-        return maxsum
+        return left_sum + right_sum
 
 
 def main():
-    print("Hello")
+    o = MaxTeilSum()
+    inputList = []
+    randomNum = random.randint(10, 50)
+    for i in range(randomNum):
+        randomInt = random.randint(-100, 100)
+        inputList.append(randomInt)
+
+    print("Input List:", inputList)
+    print("Maximum Subarray Sum:", o.findMaxSubArray(inputList))
 
 
-o = MaxTeilSum()
-
-inputList = []
-randomNum = random.randint(10, 50)
-for i in range(randomNum):
-    randomInt = random.randint(-100, 100)
-    inputList.append(randomInt)
-
-print(inputList)
-print(o.findMaxSubArray(inputList))
-
-# Main code
 if __name__ == "__main__":
     main()
